082a3996a98ee5c4ec9d54b243d4faee8907570b
kersh1337228 <antonycherevko@gmail.com>
2024-04-30 01:13:26 +0300

    feat(client): - fixed client dockerfile.

---
+++ frontend/app/post/edit/page.tsx
@@ -0,0 +1,9 @@
+'use server';
+
+import EditForm from '../../../src/components/post/form/EditForm';
+
+export default async function EditPost() {
+    return <main className="fill">
+        <EditForm />
+    </main>;
+}
